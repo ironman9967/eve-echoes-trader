0c221c5395c8de1ee6d664570eff9b65ac3172f2
Tyson Hester <ironman9967@gmail.com>
2020-12-02 20:21:47 -0500

    WIP: docker compose and mongodb

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const fetch = require('node-fetch')
 const MiniSearch = require('minisearch')
 const mathjs = require('mathjs')
 const Hapi = require('@hapi/hapi')
+const MongoClient = require('mongodb').MongoClient
 
 const _get = require('lodash/fp/get')
 const _set = require('lodash/fp/set')
@@ -55,32 +56,57 @@ cli.main((args, {
 	name
 }) => {
 	cli.debug(`${appName} v${version}`)
-	cli.debug('loading market...')
-	const started = Date.now()
-	createMarket({ MiniSearch, fetcher, cacher, queue, cacheMaxAge }).load()
-	.then(market => {
-		cli.debug(`market loaded (${Date.now() - started}ms)`)
-		switch (cli.command) {
-			case 'serve':
-				createServer({ Hapi, port, market }).start()
-					.then(() => cli.info(`server up at http://localhost:${port}/`))
-				break
-			case 'item-search':
-				market.search(term)
-					.then(res => console.log(useJson ? JSON.stringify(res) : res))
-				break
-			case 'item-names':
-				market.getItemNames()
-					.then(res => console.log(useJson ? JSON.stringify(res) : res))
-				break
-			case 'item-by-id':
-				market.getItemByItemId(itemid)
-					.then(res => console.log(useJson ? JSON.stringify(res) : res))
-				break
-			case 'item-by-name':
-				market.getItem(name)
-					.then(res => console.log(useJson ? JSON.stringify(res) : res))
-				break
-		}
-	})
+	const {
+		MONGO_INITDB_ROOT_USERNAME: mongoUser,
+		MONGO_INITDB_ROOT_PASSWORD: mongoPwd
+	} = process.env
+	const close = (code, client) => Promise.resolve(client
+		? client.close()
+		: void 0).then(() => code)
+
+	// const uri = `mongodb+srv://${mongoUser}:${mongoPwd}@localhost:27017?retryWrites=true&w=majority`
+	// const uri = `mongodb://${mongoUser}:${mongoPwd}@lednet:27017/eetdata?authSource=admin`
+	const uri = `mongodb://lednet:27017`
+
+	console.log(uri)
+
+	MongoClient.connect(uri)
+		.then(client => {
+			console.log('connected')
+			return close(0, client)
+		})
+		.catch((err, ...args) => {
+			console.error(err)
+			return close(1)
+		})
+		.then((code = 0) => process.exit(code))
+
+	// cli.debug('loading market...')
+	// const started = Date.now()
+	// createMarket({ MiniSearch, fetcher, cacher, queue, cacheMaxAge }).load()
+	// .then(market => {
+	// 	cli.debug(`market loaded (${Date.now() - started}ms)`)
+	// 	switch (cli.command) {
+	// 		case 'serve':
+	// 			createServer({ Hapi, port, market }).start()
+	// 				.then(() => cli.info(`server up at http://localhost:${port}/`))
+	// 			break
+	// 		case 'item-search':
+	// 			market.search(term)
+	// 				.then(res => console.log(useJson ? JSON.stringify(res) : res))
+	// 			break
+	// 		case 'item-names':
+	// 			market.getItemNames()
+	// 				.then(res => console.log(useJson ? JSON.stringify(res) : res))
+	// 			break
+	// 		case 'item-by-id':
+	// 			market.getItemByItemId(itemid)
+	// 				.then(res => console.log(useJson ? JSON.stringify(res) : res))
+	// 			break
+	// 		case 'item-by-name':
+	// 			market.getItem(name)
+	// 				.then(res => console.log(useJson ? JSON.stringify(res) : res))
+	// 			break
+	// 	}
+	// })
 })
